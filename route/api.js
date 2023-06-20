1c3250aa59f13f04d273eebfdc1ed69ff656314c
irivera2116 <irivera2116@GMAIL.COM>
2023-06-20 13:24:27 -0400

    fixed issues with spelling

--- route/api.js
+++ route/api.js
@@ -1,27 +1,27 @@
-const route = require('express').Router();
-const fs = require('fs');
+const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 
-route.get('/api/notes', async (req, res) => {
-    const dbJson = JSON.parse(fs.readFileSync('db/db,json', 'utf8'));
+router.get('/api/notes', async (req, res) => {
+    const dbJson = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
     res.json(dbJson);
 });
 
 // Post Note
-route.post('/api/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
     const dbJson = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
     const newPost = {
         title: req.body.title,
         text: req.body.text,
-        id: uuidv4()
+        id: uuidv4(),
     };
     dbJson.push(newPost);
-    fs.writeFileSync('db/db.json', JSON.stringify(dbJson));
+    writeFileSync('db/db.json', JSON.stringify(dbJson));
     res.json(dbJson);
 });
 
 // Delete Note
-route.delete('/api/notes/:id', (req, res) => {
+router.delete('/api/notes/:id', (req, res) => {
     let data = fs.readFileSync('db/db.json', 'utf8');
     const dataJSON = JSON.pearse(data);
     const newPosts = dataJSON.filter((note) => {
@@ -31,4 +31,4 @@ route.delete('/api/notes/:id', (req, res) => {
     res.json('Deleted Post');
 });
 
-module.exports.route;
+module.exports = router;
--- route/html.js
+++ route/html.js
@@ -1,12 +1,12 @@
-const route = require('express').Router();
+const router = require('express').Router();
 const path = require('path');
 
-route.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res/sendFile(path.json(__dirname, '../public/index.html'));
 });
 
-route.get('/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-module.expors = route;
+module.exports = router;
